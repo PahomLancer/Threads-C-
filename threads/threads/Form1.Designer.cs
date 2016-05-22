222da0864220dad07058f8eac78d41ceaec4af96
Didenko Andrey <mozggg1@gmail.com>
2016-05-22 21:08:44 +0700

    Первый апдейт

---
+++ threads/threads/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace threads
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.progressBar1.Location = new System.Drawing.Point(0, 0);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.progressBar1.Maximum = 4000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(597, 28);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 36);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(198, 36);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 28);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Pause";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(381, 36);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(216, 28);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Continue";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox1.Location = new System.Drawing.Point(0, 69);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(597, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 91);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ threads/threads/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace threads
+{
+    public partial class Form1 : Form
+    {
+        private List<WorkThread> workThreads = new List<WorkThread>();
+        private int result;
+
+        private delegate void formShowResult();
+        private delegate void formUpdateProgress(int progr);
+
+
+        private void formShowResultImpl()
+        {
+            this.textBox1.Text = "" + this.result;
+        }
+
+        private void formUpdateProgressImpl(int i)
+        {
+            this.progressBar1.Value+=i;
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.result = 0;
+            this.workThreads.Clear();
+
+            for (int i = 1; i <= 4; i++)
+            {
+                WorkThread threadImpl = new WorkThread(new Action<int>(this.workThreadCallback), new Action<WorkThread>(this.workThreadSuccessCallback));
+                threadImpl.thread.Start();
+
+                this.workThreads.Add(threadImpl);
+            }
+        }
+
+
+        private void workThreadCallback(int input)
+        {
+            formUpdateProgress progress = new formUpdateProgress(this.formUpdateProgressImpl);
+            this.Invoke(progress, new object[] { input });
+        }
+
+        private void workThreadSuccessCallback(WorkThread input)
+        {
+            this.workThreads.Remove(input);
+
+            this.result += input.result;
+
+            if (this.workThreads.Count == 0)
+            {
+                
+                formShowResult method = new formShowResult(this.formShowResultImpl);
+                this.Invoke(method);
+            }
+        }
+
+        
+        class WorkThread
+        {
+            public Thread thread { get; set; }
+            public int result { get; set; }
+            private Action<int> progressCallback;
+            private Action<WorkThread> successCallback;
+            
+            public WorkThread(Action<int> progressCallback, Action<WorkThread> successCallback)
+            {
+                this.progressCallback = progressCallback;
+                this.successCallback = successCallback;
+                this.thread = new Thread(proc);
+            }
+
+            private void proc()
+            {
+                int sum = 0;
+
+                for (int i = 0; i < 1000; i++)
+                {
+                    sum += i+1;
+
+                    if (i % 20 == 0) 
+                    {
+                        this.progressCallback.Invoke(20);
+                    }
+
+                    Thread.Sleep(20);
+                }
+
+                this.result = sum;
+                this.successCallback.Invoke(this);
+            }
+        
+        
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            foreach (WorkThread workThread in this.workThreads)
+            {
+                workThread.thread.Suspend();
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            foreach (WorkThread workThread in this.workThreads)
+            {
+                workThread.thread.Resume();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            foreach (WorkThread workThread in this.workThreads)
+            {
+                workThread.thread.Abort();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+
+     
+}
+
+
